fbaf1df2094e34bbb496595434f24f2e1a6171f9
Victor Kumar Barua <victor.tts@hotmail.com>
2024-03-28 19:07:23 +0530

    code clean and replace to switch case from if/else

--- WeatherForecast.Models/WeatherDailyForecast.cs
+++ WeatherForecast.Models/WeatherDailyForecast.cs
@@ -8,7 +8,6 @@ namespace WeatherForecast.Models
         public DateTime Date { get; private set; }
 
         [Required(ErrorMessage = "Temperature field is required.")]
-        //[Range(-60, 60, ErrorMessage = "Temperature must be between -60 and +60 degrees.")]
         public double Temperature { get; private set; }
 
         public string TemperatureDescription { get; private set; }
@@ -16,16 +15,6 @@ namespace WeatherForecast.Models
 
         public WeatherDailyForecast(DateTime date, double temperature)
         {
-            //if (date < DateTime.Now)
-            //{
-            //    throw new ArgumentOutOfRangeException(nameof(date), DomainConstant.ErrMsgDateRange);
-            //}
-
-            //if (temperature < -60 || temperature > 60)
-            //{
-            //    throw new ArgumentOutOfRangeException(nameof(temperature), DomainConstant.ErrMsgTemperatureRange);
-            //}
-
             Date = date;
             Temperature = temperature;
             TemperatureDescription = GetTemperatureDescription(temperature);
@@ -34,45 +23,28 @@ namespace WeatherForecast.Models
         // Logic to convert temperature to a a human readable way (e.g., "Freezing", "Hot", etc)
         public string GetTemperatureDescription(double temperature)
         {
-            if (temperature <= -10)
+            switch (temperature)
             {
-                return DomainConstant.constStrFreezing;
-            }
-            else if (temperature <= 0)
-            {
-                return DomainConstant.constStrBracing;
-            }
-            else if (temperature <= 10)
-            {
-                return DomainConstant.constStrChilly;
-            }
-            else if (temperature <= 20)
-            {
-                return DomainConstant.constStrCool;
-            }
-            else if (temperature <= 25)
-            {
-                return DomainConstant.constStrMild;
-            }
-            else if (temperature <= 30)
-            {
-                return DomainConstant.constStrWarm;
-            }
-            else if (temperature <= 35)
-            {
-                return DomainConstant.constStrBalmy;
-            }
-            else if (temperature <= 40)
-            {
-                return DomainConstant.constStrHot;
-            }
-            else if (temperature <= 45)
-            {
-                return DomainConstant.constStrSweltering;
-            }
-            else
-            {
-                return DomainConstant.constStrScorching;
+                case double n when n < -10:
+                    return DomainConstant.constStrFreezing; ;
+                case double n when n <= 0:
+                    return DomainConstant.constStrBracing; ;
+                case double n when n <= 10:
+                    return DomainConstant.constStrChilly;
+                case double n when n <= 20:
+                    return DomainConstant.constStrCool; ;
+                case double n when n <= 25:
+                    return DomainConstant.constStrMild;
+                case double n when n <= 30:
+                    return DomainConstant.constStrWarm;
+                case double n when n <= 35:
+                    return DomainConstant.constStrBalmy;
+                case double n when n <= 40:
+                    return DomainConstant.constStrHot;
+                case double n when n > 45:
+                    return DomainConstant.constStrSweltering;
+                default:
+                    return DomainConstant.constStrScorching; ;
             }
         }
     }
